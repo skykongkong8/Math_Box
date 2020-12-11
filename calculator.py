03e100233524062603e2ecca8342855baefcb55c
sungsik <kssjustin98@gmail.com>
2020-12-11 11:22:46 +0900

    Add divide and multuply functions

--- calculator.py
+++ calculator.py
@@ -3,4 +3,10 @@ def add(a,b):
     return a+b
 def subtract(a,b):
     return a-b
-
+def multiply(a,b):
+    return a*b
+def divide(a,b):
+    if b !=0:
+        return a/b
+    else:
+        return 'ZeroDivisionError'
